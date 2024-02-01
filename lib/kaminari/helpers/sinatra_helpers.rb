902d79cfd9089c690fe59c6f5e9814ee9a12d25a
Shuhei Kinukawa <kinushu@gmail.com>
2024-02-01 14:52:28 +0900

    fix Ruby3.0 env

--- lib/kaminari/helpers/sinatra_helpers.rb
+++ lib/kaminari/helpers/sinatra_helpers.rb
@@ -93,7 +93,7 @@ module Kaminari::Helpers
 
         template = ActionViewTemplateProxy.new current_params: current_params, current_path: current_path, param_name: options[:param_name] || Kaminari.config.param_name
 
-        super scope, {template: template}.merge(options)
+        super scope, **{template: template}.merge(options)
       end
     end
     Kaminari::Helpers::HelperMethods.prepend SinatraHelperMethods
